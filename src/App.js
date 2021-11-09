f8e0ac6a9470d657d5b9727668448404456f4f59
Mateusz Choma <kontakt@coderoad.pl>
2021-11-09 18:46:19 +0100

    Form added with uncontrolled inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react'
 
 export const App = () => {
+  console.log('RENDER')
+
   return (
     <div>
-      CodeRoad APP
+      <h1>
+        Create account
+      </h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          console.log(e)
+
+          const form = e.target
+          const formData = new FormData(form)
+
+          console.log(formData.get('email'))
+          console.log(formData.get('password'))
+          console.log(formData.get('repeatPassword'))
+        }}
+      >
+        <input
+          placeholder={'E-mail'}
+          name={'email'}
+        />
+        <br />
+        <input
+          placeholder={'Password'}
+          name={'password'}
+        />
+        <br />
+        <input
+          placeholder={'Repeat rssword'}
+          name={'repeatPassword'}
+        />
+        <br />
+        <button>
+          SUBMIT
+        </button>
+      </form>
     </div>
   )
 }
